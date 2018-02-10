a71468e2f18f9b81829ae37d9ca3bbbeca534ad1
Stenio das Neves Silva <stenionevessilva@gmail.com>
2018-02-10 19:39:56 -0300

    Inicio de desenvolvimento do sistema 10/02/2018 1939

---
+++ src/main/java/org/sisger/conf/AppWebConfiguration.java
@@ -0,0 +1,31 @@
+package org.sisger.conf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/***
+ * Classe de configuração  do Spring equivale ao web.xml, contem as configurações do sistema.
+ * @author stenio
+ *
+ */
+@EnableWebMvc
+@ComponentScan(basePackages="org.sisger.controllers")
+public class AppWebConfiguration { 
+	
+/***
+ * método de configuração de localização das views e a extensão do arquivo
+ * @return resolver
+ */
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver(){
+	
+		InternalResourceViewResolver resolver =
+				new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+}
---
+++ src/main/java/org/sisger/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package org.sisger.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/***
+ * Classe de gerenciamento da views principais.
+ * @author stenio
+ *
+ */
+@Controller
+public class HomeController {	
+	
+	@RequestMapping("/")
+	public ModelAndView index(){
+		 ModelAndView andView =new 
+		return ;
+		
+	}
+	
+
+}
